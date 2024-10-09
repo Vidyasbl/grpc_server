ca6e44c4611b1269c04e755c8e400ee2cc0d6d55
Vidyashree <vidya.sbl28@gmail.com>
2024-10-09 05:32:58 +0530

    add rest server too

--- server.js
+++ server.js
@@ -1,41 +1,52 @@
-const grpc = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader');
+// const grpc = require('@grpc/grpc-js');
+// const protoLoader = require('@grpc/proto-loader');
 
-const PROTO_PATH = __dirname + '/greet.proto';
+// const PROTO_PATH = __dirname + '/greet.proto';
 
-const packageDefinition = protoLoader.loadSync(
-  PROTO_PATH,
-  {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true
-  }
-);
+// const packageDefinition = protoLoader.loadSync(
+//   PROTO_PATH,
+//   {
+//     keepCase: true,
+//     longs: String,
+//     enums: String,
+//     defaults: true,
+//     oneofs: true
+//   }
+// );
 
-const greet = grpc.loadPackageDefinition(packageDefinition);
+// const greet = grpc.loadPackageDefinition(packageDefinition);
 
-function sayHello(call, callback) {
-  callback(null, { message: 'Hello, ' + call.request.name + '!' });
-}
+// function sayHello(call, callback) {
+//   callback(null, { message: 'Hello, ' + call.request.name + '!' });
+// }
 
-const server = new grpc.Server();
-server.addService(greet.Greeter.service, { sayHello });
-// home wifi
-// server.bindAsync('192.168.0.102:50051', grpc.ServerCredentials.createInsecure(), (error)=> {
+// const server = new grpc.Server();
+// server.addService(greet.Greeter.service, { sayHello });
+// // home wifi
+// // server.bindAsync('192.168.0.102:50051', grpc.ServerCredentials.createInsecure(), (error)=> {
 
-// vidya hotspot, emulator
-// server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), (error)=> {
+// // vidya hotspot, emulator
+// // server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), (error)=> {
 
+// const port = process.env.PORT || 4000;
+// server.bindAsync('0.0.0.0:' + port, grpc.ServerCredentials.createInsecure(), (error) => {
+//   if (error != null) {
+//     console.log('error:');
+//     console.log(error); 
+//   }
+//   server.start();
+//   console.log('gRPC server running on port ' + port);
+// });
+
+
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 4000;
-server.bindAsync('0.0.0.0:' + port, grpc.ServerCredentials.createInsecure(), (error) => {
-  if (error != null) {
-    console.log('error:');
-    console.log(error); 
-  }
-  server.start();
-  console.log('gRPC server running on port ' + port);
+
+app.get('/hello/:name', (req, res) => {
+  res.send(`Hello, ${req.params.name}!`);
 });
 
-
+app.listen(port, () => {
+  console.log(`REST server running on port ${port}`);
+});
